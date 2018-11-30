3336330ddc4b1b0063f575a5b5153df42e8cf1fe
rogercorp <42458303+rogercorp@users.noreply.github.com>
2018-11-29 22:31:33 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,14 +8,14 @@ variable "gcp_project" {
 
 variable "gcp_region" {
   description = "GCP region, e.g. us-east1"
-  default = "us-east1"
+  default = "us-west1"
 }
 
 variable "gcp_zone" {
   description = "GCP zone, e.g. us-east1-a"
-  default = "us-east1-b"
+  default = "us-west1-b"
 }
-
+east1
 variable "machine_type" {
   description = "GCP machine type"
   default = "n1-standard-1"
@@ -23,7 +23,7 @@ variable "machine_type" {
 
 variable "instance_name" {
   description = "GCP instance name"
-  default = "demo"
+  default = "rogmanster"
 }
 
 variable "image" {
